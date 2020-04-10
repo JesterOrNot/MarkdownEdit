bcb4d4b679348fba3378635b0d3f8538786ecc0a
Restyled.io <commits@restyled.io>
2020-04-10 02:08:38 +0000

    Restyled by clang-format

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const { markdown_to_html } = wasm_bindgen;
+const {markdown_to_html} = wasm_bindgen;
 async function run() {
-    await wasm_bindgen('./pkg/markdown_edit_bg.wasm');
-    let textfield = document.querySelector("#editor");
-    renderMarkdown(textfield, markdown_to_html);
+  await wasm_bindgen('./pkg/markdown_edit_bg.wasm');
+  let textfield = document.querySelector("#editor");
+  renderMarkdown(textfield, markdown_to_html);
 }
 function renderMarkdown(textfield, markdown_render) {
-    textfield.oninput = function() {
-        inputContent.innerHTML = markdown_render(this.value);
-    }
+  textfield.oninput =
+      function() { inputContent.innerHTML = markdown_render(this.value); }
 }
 run();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(_ => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
+    navigator.serviceWorker.register("/serviceWorker.js")
+        .then(_ => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
   })
 }
