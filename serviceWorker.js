aaa336a622ac537031ee4048d83f1bd2378d1165
Restyled.io <commits@restyled.io>
2020-04-10 02:08:41 +0000

    Restyled by prettier

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,30 @@
-const MarkdownEdit = "MarkdownEdit"
+const MarkdownEdit = "MarkdownEdit";
 const assets = [
-  "/", "/index.html", "/styles.css", "/app.js", "/pkg", "/pkg/package.json",
-  "/pkg/markdown_edit.js", "/pkg/markdown_edit.d.ts",
-  "/pkg/markdown_edit_bg.wasm", "/pkg/markdown_edit_bg.d.ts", "/pkg/.gitignore",
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/pkg",
+  "/pkg/package.json",
+  "/pkg/markdown_edit.js",
+  "/pkg/markdown_edit.d.ts",
+  "/pkg/markdown_edit_bg.wasm",
+  "/pkg/markdown_edit_bg.d.ts",
+  "/pkg/.gitignore",
   "/pkg/README.md"
-]
+];
 
-self.addEventListener(
-    "install",
-    installEvent => {installEvent.waitUntil(
-        caches.open(MarkdownEdit).then(cache => {cache.addAll(assets)}))})
-self.addEventListener(
-    "fetch", fetchEvent => {fetchEvent.respondWith(
-                 caches.match(fetchEvent.request)
-                     .then(res => {return res || fetch(fetchEvent.request)}))})
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(MarkdownEdit).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
